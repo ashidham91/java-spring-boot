c8467c580761470cdebde00b7be885c3c92d89ed
ashidham <ashidham91@gmail.com>
2024-05-09 11:54:06 +0530

    first commit

---
+++ src/main/java/com/java/javaspringboot/JavaSpringBootApplication.java
@@ -0,0 +1,20 @@
+package com.java.javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaSpringBootApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Application Deployed Successfully!!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootApplication.class, args);
+	}
+
+}
